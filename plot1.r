7edc377aac190e7b8e030d69edd20d6f50f6980d
frmj <cao_ricardo@hotmail.com>
2014-05-10 17:08:53 -0700

    Update plot1.r

--- plot1.r
+++ plot1.r
@@ -1,6 +1,7 @@
 #we only want to read the data from 2007-02-01 to 2007-02-02 and because
 #the file is so big we must do the following to only read what we want
 
+
 data <- read.table("household_power_consumption.txt", header=T, sep=";")
 data$Date <- as.Date(data$Date, format='%d/%m/%Y')
 dataDate <- data[data$Date %in% as.Date(c('2007-02-01', '2007-02-02')),]
@@ -10,6 +11,7 @@ attach(dataDate)
 
 nrow(dataDate)
 head(dataDate)
+
 #------------------
 #plot 1
 png(filename = "plot1.png", width = 480, height = 480)
